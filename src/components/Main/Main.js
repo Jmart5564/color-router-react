9c73994583ddbcb575cee21d5fce9c4267ed9fc8
Jmart5564 <Jmart5564@gmail.com>
2022-09-27 14:58:06 -0700

    added navigation component and links work and change background based on color name

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,9 +1,22 @@
-import RGB from './components/RGB/RGB';
+import RGB from '../RGB/RGB';
+import NavLinks from '../NavLinks/NavLinks';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 function Main() {
   return (
-    <span>Hello</span>
+    <div>
+      <Switch>
+        <Route exact path="/">
+          <Redirect to="/rgb/34/139/34" />
+        </Route>
+        <Route exact path="/rgb/:r/:g/:b">
+          <NavLinks />
+          <RGB />
+        </Route>
+      </Switch>
+    </div>
   );
+  
 }
   
 export default Main;
